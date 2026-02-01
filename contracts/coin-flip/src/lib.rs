79da5884ccd4014e2a533ca1fbc87fe56a60636d
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2026-02-01 10:45:23 +0100

    feat: add coin-flip soroban contract skeleton

---
+++ contracts/coin-flip/src/lib.rs
@@ -0,0 +1,27 @@
+//! Stellarcade Coin Flip Contract
+//! 
+//! Implements the classic 50/50 game logic.
+#![no_std]
+
+use soroban_sdk::{contract, contractimpl, Address, Env, BytesN};
+
+#[contract]
+pub struct CoinFlip;
+
+#[contractimpl]
+impl CoinFlip {
+    /// Play the coin flip game.
+    pub fn play(env: Env, player: Address, amount: i128, choice: u32, seed: BytesN<32>) {
+        // TODO: Call PrizePool to lock/deposit amount
+        // TODO: Call RandomGenerator to get result
+        // TODO: Determine if choice (0 or 1) matches result
+        // TODO: If win, call PrizePool to pay out win amount
+        // TODO: Emit CoinFlipResult event
+    }
+
+    /// View previous game result for verification.
+    pub fn get_game_result(env: Env, game_id: u32) -> u32 {
+        // TODO: Retrieve result from storage
+        0
+    }
+}
